43ff6d22da514911fc1cfee172b4155bf92868c7
Farshad Hasanpour <farshad.hasanpour96@gmail.com>
2025-01-05 17:08:09 +0330

    removed csrf

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 use Illuminate\Support\Facades\Route;
+use \Illuminate\Foundation\Http\Middleware\ValidateCsrfToken;
 
 use App\Http\Controllers\RoomController;
 use App\Http\Controllers\GuestController;
 use App\Http\Controllers\ReservationController;
 
-Route::apiResource('rooms', RoomController::class);
-Route::apiResource('guests', GuestController::class);
-Route::apiResource('reservations', ReservationController::class);
+Route::withoutMiddleware([ValidateCsrfToken::class])->group(function(){
+    Route::apiResource('rooms', RoomController::class);
+    Route::apiResource('guests', GuestController::class);
+    Route::apiResource('reservations', ReservationController::class);
+});
+
